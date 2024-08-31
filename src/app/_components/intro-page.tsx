082927a42d902006cd20b41023f1f29b40bb1956
Ye Fan <yefancs@gmail.com>
2024-08-31 15:33:45 -0700

    fixed a escape character error

--- src/app/_components/intro-page.tsx
+++ src/app/_components/intro-page.tsx
@@ -13,7 +13,7 @@ const IntroPage = (show: boolean, updatePart: (newPart: number) => void) =>{
                 Real heroism is not showing that you can handle anything and everything that comes your way. Rather, it is doing your own part, however humble that might be. -- Carol S. Pearson
             </div>
             <div className={styles.text} style={{ maxWidth: '70%', textAlign: 'center', marginTop: '20px' }}>
-                This heroic myth index is a convenient tool from Dr. Pearson's book <i><a href="https://www.amazon.com/Awakening-Heroes-Within-Archetypes-Ourselves/dp/0062506781/" target="_blank" rel="noopener noreferrer">Awakening the hero within</a></i>. It reveals the current active archetype, and where one is at on their heroic journey.
+                This heroic myth index is a convenient tool from Dr. Pearson&apos;s book <i><a href="https://www.amazon.com/Awakening-Heroes-Within-Archetypes-Ourselves/dp/0062506781/" target="_blank" rel="noopener noreferrer">Awakening the hero within</a></i>. It reveals the current active archetype, and where one is at on their heroic journey.
             </div>
             <div style={{ marginTop: '20px' }}>
                 <Button name="next" variant="contained" onClick={() => updatePart(1)}>Take the questionnaire</Button>
